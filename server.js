e0035ca038aeee52af52ab51b3059ca41540354e
unknown <khhan1990@hanmail.net>
2022-06-23 22:03:12 +0900

    sqlite 이용하여 데이터 넣고 조회

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const models = require("./models");
 const port = 8080;
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/products", (req, res) => {
-  const { query } = req;
-  console.log("QUERY : ", query);
-  res.send({
-    products: [
-      {
-        id: 1,
-        name: "농구공",
-        price: 10000,
-        seller: "조던",
-        imageUrl: "images/products/basketball1.jpeg",
-      },
-      {
-        id: 2,
-        name: "축구공",
-        price: 50000,
-        seller: "조던",
-        imageUrl: "images/products/soccerball1.jpg",
-      },
-      {
-        id: 3,
-        name: "키보드",
-        price: 15000,
-        seller: "규호",
-        imageUrl: "images/products/keyboard1.jpg",
-      },
-    ],
-  });
+  models.Product.findAll()
+    .then((result) => {
+      console.log("PRODUCTS :", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("에러 발생");
+    });
 });
 
 app.post("/products", (req, res) => {
   const { body } = req;
-  res.send({
-    body,
-  });
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해 주세요.");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과 :", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 업로드에 문제가 발생했습니다.");
+    });
 });
 
 app.get("/products/:id", (req, res) => {
@@ -52,4 +54,13 @@ app.get("/products/:id", (req, res) => {
 
 app.listen(port, () => {
   console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((err) => {
+      console.error("DB 연결 에러 : ", error);
+      process.exit();
+    });
 });
